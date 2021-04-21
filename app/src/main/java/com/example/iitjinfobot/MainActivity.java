f09a9e63a66950c8ad35c9513c01017fb084c22e
imraghav20 <raghav20nov@gmail.com>
2021-04-21 17:30:53 +0530

    added function of adding messages in scroll view

--- app/src/main/java/com/example/iitjinfobot/MainActivity.java
+++ app/src/main/java/com/example/iitjinfobot/MainActivity.java
@@ -4,12 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
     GoogleSignInClient mGoogleSignInClient;
 
     private TextView helloUser;
+    private LinearLayout chat_layout;
+    private EditText message;
+    private ImageButton send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
         GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
 
         helloUser = findViewById(R.id.user_hello);
+        chat_layout = findViewById(R.id.chat_layout);
+        message = findViewById(R.id.message);
+        send = findViewById(R.id.send_button);
 
         if (acct != null) {
 //            String personName = acct.getDisplayName();
@@ -51,6 +58,21 @@ public class MainActivity extends AppCompatActivity {
             String greetingText = helloUser.getText().toString();
             greetingText += " Hello\n" + personGivenName;
             helloUser.setText(greetingText);
+
+            TextView tv = new TextView(this);
+            LinearLayout.LayoutParams lParams = new LinearLayout.LayoutParams(480, LinearLayout.LayoutParams.WRAP_CONTENT);
+            lParams.setMargins(6, 6, 6, 6);
+            tv.setLayoutParams(lParams);
+            tv.setText("Hello " + personGivenName + ". I am your IITJ Assistant. How can I help you?");
+            tv.setTextSize(16);
+            tv.setPadding(6, 6, 6, 6);
+            Drawable img = getApplicationContext().getResources().getDrawable(R.drawable.logo);
+            img.setBounds(0, 0, 60, 60);
+            tv.setCompoundDrawables(img, null, null, null);
+
+            tv.setBackgroundResource(R.drawable.bot_text);
+            chat_layout.addView(tv);
+
         } else {
             Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
             loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
